2b167bff39afc585893cbb80d53308713427eb24
Milumon <17993682+Milumon@users.noreply.github.com>
2021-01-14 02:21:56 -0500

    Add HTML + CSS + JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,79 @@
+let stringResult = '';
+
 const cipher = {
-  // ...
+  
+  encode: function (offset, string) {
+
+
+    let words = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+    stringResult = '';
+
+
+    if (typeof (offset) !== 'number') {
+      throw TypeError('El offset debe ser un número');
+
+    } else if (typeof (string) !== 'string') {
+      throw TypeError('El string debe ser un string');
+    }
+
+    for (var i = 0; i < string.length; i++) {
+      let letter = string[i];
+      let validWord = words.includes(letter.toUpperCase());
+ 
+
+      if (validWord == true) {
+        if (letter == letter.toUpperCase()) {
+          let ASCIIvalue = (letter.charCodeAt(0) - 65 + offset) % 26 + 65;
+          let finalLetter = String.fromCharCode(ASCIIvalue);
+          stringResult = stringResult + finalLetter;
+        } else {
+          let ASCIIvalue = (letter.charCodeAt(0) - 97 + offset) % 26 + 97;
+          let finalLetter = String.fromCharCode(ASCIIvalue);
+          stringResult = stringResult + finalLetter;
+        }
+      } else {
+        stringResult = stringResult + letter;
+      } 
+    }
+    return stringResult;
+  },
+
+  decode: function (offset, string) {
+
+    let words = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+     stringResult = '';
+
+
+    if (typeof (offset) !== 'number') {
+      throw TypeError('El offset debe ser un número');
+
+    } else if (typeof (string) !== 'string') {
+      throw TypeError('El string debe ser un string');
+    }
+
+    for (var i = 0; i < string.length; i++) {
+      let letter = string[i];
+      let validWord = words.includes(letter.toUpperCase());
+
+      if (validWord == true) {
+        if (letter == letter.toUpperCase()) {
+          let ASCIIvalue = (letter.charCodeAt(0) + 65 - offset ) % 26 + 65;
+          let finalLetter = String.fromCharCode(ASCIIvalue);
+          stringResult = stringResult + finalLetter;
+        } else {
+          let ASCIIvalue = (letter.charCodeAt(0) - 97 - offset + 26) % 26  + 97 ;
+          let finalLetter = String.fromCharCode(ASCIIvalue);
+          stringResult = stringResult + finalLetter;
+        }
+
+      } else {
+        stringResult = stringResult + letter;
+      }
+    }
+    return stringResult;
+  } 
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
+/* eslint-disable no-console */
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+let encodeButton = document.getElementById('btnEncode');
+encodeButton.addEventListener('click', cipherEncode);
+
+let decodeButton = document.getElementById('btnDecode');
+decodeButton.addEventListener('click', cipherDecode);
+
+function cipherEncode() {
+  // obtener el valor del offset
+  let offsetValue = parseInt(document.getElementById('offset').value);
+  // obtener el valor del string
+  let stringValue = document.getElementById('decode').value;
+  // obtener el resultado del descifrado
+  let textEncode = cipher.encode(offsetValue, stringValue);
+  // asignar el valor al textArea
+  document.getElementById('encoded').value = textEncode;
+}
+
+
+
+function cipherDecode() {
+  // obtener el valor del offset
+  let offsetValue = parseInt(document.getElementById('offset').value);
+  // obtener el valor del string
+  let stringValue = document.getElementById('decode').value;
+  // obtener el resultado del descifrado
+  let textEncode = cipher.decode(offsetValue, stringValue);
+  // asignar el valor al textArea
+  document.getElementById('encoded').value = textEncode;
+}
